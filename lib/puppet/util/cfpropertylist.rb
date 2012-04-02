3a0e5418d2dedf26780b7ccc85daf3b4f2baba6c
Gary Larizza <gary@puppetlabs.com>
2012-04-02 13:45:11 -0700

    Fix cfpropertylist.rb
    I had omitted the lib directory in the require

--- lib/puppet/util/cfpropertylist.rb
+++ lib/puppet/util/cfpropertylist.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-require File.join(File.dirname(__FILE__), 'cfpropertylist', 'rbCFPropertyList.rb')
+require File.join(File.dirname(__FILE__), 'cfpropertylist', 'lib', 'rbCFPropertyList.rb')
 
 
 # eof
